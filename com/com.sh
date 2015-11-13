1cd815016a9179473fda4fcce4db3566b58b58f9
fabien.lementec <fabien.lementec@gmail.com>
2015-11-13 16:28:45 +0100

    add: vcom compilation script

---
+++ com/com.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+vlib work
+
+vcom -quiet ../src/absenc_pkg.vhd
+vcom -quiet ../src/absenc_master_biss.vhd
+vcom -quiet ../src/absenc_master_ssi.vhd
+vcom -quiet ../src/absenc_master_endat.vhd
+vcom -quiet ../src/absenc_master.vhd
+vcom -quiet ../src/absenc_slave_biss.vhd
+vcom -quiet ../src/absenc_slave_ssi.vhd
+vcom -quiet ../src/absenc_slave_endat.vhd
+vcom -quiet ../src/absenc_slave.vhd
